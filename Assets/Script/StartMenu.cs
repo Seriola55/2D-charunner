b6d4f3941761d2333ca4cada77ac98652ac5bdb9
Seriola55 <mf8hh5jc88@privaterelay.appleid.com>
2026-06-15 20:58:26 +0900

    シーン遷移

--- Assets/Script/StartMenu.cs
+++ Assets/Script/StartMenu.cs
@@ -11,6 +11,18 @@ public class StartMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Course2");
     }
+    public void StartGame3()
+    {
+        SceneManager.LoadScene("Course3");
+    }
+    public void StartGame4()
+    {
+        SceneManager.LoadScene("Course4");
+    }
+    public void StartGame5()
+    {
+        SceneManager.LoadScene("Course5");
+    }
     public void GoToMenu()
     {
         Time.timeScale=1f;
